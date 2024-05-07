53bd5b50e2373a2af2d9d08ac50bf46c33ba6b99
ryanciusjmak <ryanciusjmak@gmail.com>
2024-05-06 21:01:26 -0300

    updates to event id view

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -6,14 +6,18 @@
 <div class="col-md-10 offset-md-1">
     <div class="row">
         <div id="image-container" class="col-md-6">
-            <img src="/img/events/{{ $event->image}}" alt="{{$event->title}}" class="img-fluid">
+            <img src="/img/events/{{ $event->image }}" class="img-fluid" alt="{{ $event->title }}">
         </div>
         <div id="info-container" class="col-md-6">
-            <h1>{{$event->title}}</h1>
-            <p class="event-city">
-                <ion-icon class="location-outline"></ion-icon>
-                {{$event->city}}
-            </p>
+            <h1>{{ $event->title }}</h1>
+            <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $event->city }}</p>
+            <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Participants</p>
+            <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Event Owner</p>
+            <a href="#" class="btn btn-primary" id="event-submit">Confirm Attendance</a>
+        </div>
+        <div class="col-md-12" id="description-container">
+            <h3>About the event:</h3>
+            <p class="event-description">{{ $event->description }}</p>
         </div>
     </div>
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="shortcut icon" href="/public/img/favicon.png" type="image/x-icon">
+        <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
         <script src="/js/script.js"></script>
     </head>
     <body>
