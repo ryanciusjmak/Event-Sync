3d8139c0a4eb0302e75d5e737c140d6e080fe30d
ryanciusjmak <ryanciusjmak@gmail.com>
2024-05-20 23:15:39 -0300

    styles updates

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div id="event-create-container" class="col-md-6 offset-md-3 mt-5">
-        <h1>Create your Event</h1>
+        <h1 class="text-center">Create your Event</h1>
         <form action="/events" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
@@ -27,32 +27,47 @@
                 </select>
             </div>
             <div class="form-group">
-                <label class="mt-3" for="description">Description</label>
-                <textarea class="form-control" name="description" id="description" placeholder="Description of the event"></textarea>
+    <label class="mt-3" for="description">Add Infrastructure Items</label>
+    <div class="row">
+        <div class="col-5">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="items[]" value="Chairs" id="chairs">
+                <label class="form-check-label ms-2" for="chairs">Chairs</label>
             </div>
+        </div>
+        <div class="col-5">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="items[]" value="Stage" id="stage">
+                <label class="form-check-label ms-2" for="stage">Stage</label>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="items[]" value="Open Food" id="open-food">
+                <label class="form-check-label ms-2" for="open-food">Open Food</label>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="items[]" value="Free beer" id="free-beer">
+                <label class="form-check-label ms-2" for="free-beer">Free Beer</label>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="items[]" value="Gifts" id="gifts">
+                <label class="form-check-label ms-2" for="gifts">Gifts</label>
+            </div>
+        </div>
+    </div>
+</div>
             <div class="form-group">
-                <label class="mt-3" for="description">Add Infrastructure Items</label>
-                <div class="form-group">
-                    <input type="checkbox" name="items[]" value="Chairs"> Chairs
-                </div>
-                <div class="form-group">
-                    <input type="checkbox" name="items[]" value="Stage"> Stage
-                </div>
-                <div class="form-group">
-                    <input type="checkbox" name="items[]" value="Open Food"> Open Food
-                </div>
-                <div class="form-group">
-                    <input type="checkbox" name="items[]" value="Free beer"> Free beer
-                </div>
-                <div class="form-group">
-                    <input type="checkbox" name="items[]" value="Gifts"> Gifts
-                </div>
+                <label class="mt-3 " for="image">Add Photo</label>
+                <input type="file" class="form-control-file mt-3 ms-2" id="image" name="image"> 
             </div>
-            <div class="form-group">
-                <label class="mt-3" for="image">Add Photo</label>
-                <input type="file" class="form-control-file mt-3" id="image" name="image"> 
+            <div class="text-center"> 
+                <input type="submit" class="btn btn-primary mt-3" value="Create Event" id="create-button">
             </div>
-            <input type="submit" class="btn btn-primary mt-3" value="Create Event">
         </form>
     </div>
 @endsection
